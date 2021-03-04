f4915b47c52d346eed4c7265d05c060e19eef709
Miles Grossenbacher <mrg498@nyu.edu>
2021-03-04 15:31:42 -0500

    ✨ background image

--- gatsby-project/src/components/header.js
+++ gatsby-project/src/components/header.js
@@ -1,30 +1,31 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
-import * as headerStyles from "./header.module.scss"
+import * as headerStyles from "../styles/header.module.scss";
 
 const Header = () => (
-  <header className={headerStyles.container}>
-    <div className={headerStyles.iconWrapper}>
-        Site Logo
-    </div>
-    <div className={headerStyles.largeLinksWrapper}>
-      <ul className={headerStyles.largeLinks}>
-          <li className={headerStyles.largeLink}>
-            Home
-          </li>
-          <li className={headerStyles.largeLink}>
-            About
-          </li>
-          <li className={headerStyles.largeLink}>
-            Projects
-          </li>
-          <li className={headerStyles.largeLink}>
-            Contact
-          </li>
-      </ul>
-    </div>
-  </header>
-)
+	<header className={headerStyles.container}>
+		<div className={headerStyles.iconWrapper}>
+			<StaticImage
+				src="../images/gatsby-icon.png"
+				alt="A dinosaur"
+				placeholder="blurred"
+				layout="fixed"
+				width={56}
+        height={56}
+        className={headerStyles.icon}
+			/>
+		</div>
+		<div className={headerStyles.largeLinksWrapper}>
+			<ul className={headerStyles.largeLinks}>
+				<li className={headerStyles.largeLink}>Home</li>
+				<li className={headerStyles.largeLink}>About</li>
+				<li className={headerStyles.largeLink}>Projects</li>
+				<li className={headerStyles.largeLink}>Contact</li>
+			</ul>
+		</div>
+	</header>
+);
 
-export default Header
+export default Header;
--- gatsby-project/src/pages/index.js
+++ gatsby-project/src/pages/index.js
@@ -5,8 +5,20 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import * as indexStyles from "../styles/index.module.scss"
+
 const IndexPage = () => (
   <Layout>
+    <div className={indexStyles.bgImageOverlay}/>
+    <StaticImage 
+      src="../images/jake-hills-mI02K_LxlfU-unsplash.jpg"
+      alt="background image"
+      layout='fullWidth'
+      loading="eager"
+      backgroundColor='rgb(0,0,0)'
+      quality={30}
+      className={indexStyles.bgImage}
+    />
     <SEO title="Home" />
   </Layout>
 )
