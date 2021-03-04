dd3c9eab2840e57997a2dee44b51df8caa975f34
Miles Grossenbacher <mrg498@nyu.edu>
2021-03-04 14:20:06 -0500

    🚧 creating header component

--- gatsby-project/src/components/header.js
+++ gatsby-project/src/components/header.js
@@ -1,8 +1,29 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import * as headerStyles from "./header.module.scss"
+
 const Header = () => (
-  <header>
+  <header className={headerStyles.container}>
+    <div className={headerStyles.iconWrapper}>
+        Site Logo
+    </div>
+    <div className={headerStyles.largeLinksWrapper}>
+      <ul className={headerStyles.largeLinks}>
+          <li className={headerStyles.largeLink}>
+            Home
+          </li>
+          <li className={headerStyles.largeLink}>
+            About
+          </li>
+          <li className={headerStyles.largeLink}>
+            Projects
+          </li>
+          <li className={headerStyles.largeLink}>
+            Contact
+          </li>
+      </ul>
+    </div>
   </header>
 )
 
