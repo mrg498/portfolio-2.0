8e3e6ab9643050bf49ea5c38628c1da7110dd088
Miles Grossenbacher <mrg498@nyu.edu>
2021-03-06 17:33:44 -0500

    ✨ hamburger icon

--- gatsby-project/src/components/header.js
+++ gatsby-project/src/components/header.js
@@ -1,30 +1,50 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
 import * as headerStyles from "../styles/header.module.scss";
 
-const Header = () => (
-	<header className={`${headerStyles.container} fadeIn`} style={{animationDelay: `2.5s`}}>
-		<div className={headerStyles.iconWrapper}>
-			<StaticImage
-				src="../images/gatsby-icon.png"
-				alt="A dinosaur"
-				placeholder="blurred"
-				layout="fixed"
-				width={56}
-        height={56}
-			/>
-		</div>
-		<div>
-			<ul className={`${headerStyles.largeLinks}`}>
-				<li className={headerStyles.largeLink}>Home</li>
-				<li className={headerStyles.largeLink}>About</li>
-				<li className={headerStyles.largeLink}>Projects</li>
-				<li className={headerStyles.largeLink}>Contact</li>
-			</ul>
-		</div>
-	</header>
-);
+const Header = () => {
+	const [ toggled, setToggled ] = useState(false);
+
+	const toggleOverlay = () => {
+		setToggled((toggled) => {
+			return !toggled;
+		});
+	};
+
+	return (
+		<header className={`${headerStyles.container} fadeIn`} style={{ animationDelay: `2.5s` }}>
+			<div className={headerStyles.iconWrapper}>
+				<StaticImage
+					src="../images/gatsby-icon.png"
+					alt="A dinosaur"
+					placeholder="blurred"
+					layout="fixed"
+					width={44}
+					height={44}
+				/>
+			</div>
+			<div className={headerStyles.desktop}>
+				<ul className={`${headerStyles.largeLinks}`}>
+					<li className={headerStyles.largeLink}>Home</li>
+					<li className={headerStyles.largeLink}>About</li>
+					<li className={headerStyles.largeLink}>Projects</li>
+					<li className={headerStyles.largeLink}>Contact</li>
+				</ul>
+			</div>
+			<div
+				className={toggled ? `${headerStyles.hamburger} ${headerStyles.isActive}` : `${headerStyles.hamburger}`}
+				onClick={() => {
+					toggleOverlay();
+				}}
+			>
+				<div className={headerStyles.hamburgerLine} />
+				<div className={headerStyles.hamburgerLine} />
+				<div className={headerStyles.hamburgerLine} />
+			</div>
+		</header>
+	);
+};
 
 export default Header;
