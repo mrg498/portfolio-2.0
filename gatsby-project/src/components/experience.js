4e38c4f9df02a9f05e7c4bfec87547167ab5cad6
Miles Grossenbacher <mrg498@nyu.edu>
2021-03-07 21:26:33 -0500

    🐛 wrap row experience sections

--- gatsby-project/src/components/experience.js
+++ gatsby-project/src/components/experience.js
@@ -9,61 +9,65 @@ export default function experience() {
 				<span>Experience</span>
 			</h2>
 			<div className={expStyles.postContainer}>
-				<div className={expStyles.post}>
-                    <h3>
-						<span>Analog Collective</span>
-					</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
-						dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
-						ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor aliquam
-						nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque in. Non
-						nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et magnis dis
-						parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
-					</p>
+				<div className={expStyles.row}>
+					<div className={expStyles.post}>
+						<h3>
+							<span>Analog Collective</span>
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
+							dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
+							ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor
+							aliquam nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque
+							in. Non nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et
+							magnis dis parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
+						</p>
+					</div>
+					<div className={expStyles.post}>
+						<h3>
+							<span>Handoff Tech</span>
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
+							dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
+							ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor
+							aliquam nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque
+							in. Non nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et
+							magnis dis parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
+						</p>
+					</div>
 				</div>
-				<div className={expStyles.post}>
-                    <h3>
-						<span>Handoff Tech</span>
-					</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
-						dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
-						ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor aliquam
-						nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque in. Non
-						nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et magnis dis
-						parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
-					</p>
-				</div>
-				<div className={expStyles.post}>
-                    <h3>
-						<span>Chandrika Tandon</span>
-					</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
-						dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
-						ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor aliquam
-						nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque in. Non
-						nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et magnis dis
-						parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
-					</p>
-				</div>
-				<div className={expStyles.post}>
-                    <h3>
-						<span>NYU Computer Science</span>
-					</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
-						dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
-						ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor aliquam
-						nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque in. Non
-						nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et magnis dis
-						parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
-					</p>
+				<div className={expStyles.row}>
+					<div className={expStyles.post}>
+						<h3>
+							<span>Chandrika Tandon</span>
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
+							dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
+							ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor
+							aliquam nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque
+							in. Non nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et
+							magnis dis parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
+						</p>
+					</div>
+					<div className={expStyles.post}>
+						<h3>
+							<span>NYU Computer Science</span>
+						</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Sagittis aliquam malesuada bibendum arcu vitae. Ullamcorper
+							dignissim cras tincidunt lobortis feugiat. Quis auctor elit sed vulputate mi sit. Tincidunt
+							ornare massa eget egestas purus. In cursus turpis massa tincidunt dui. Tincidunt tortor
+							aliquam nulla facilisi cras fermentum. Volutpat blandit aliquam etiam erat velit scelerisque
+							in. Non nisi est sit amet. Leo vel fringilla est ullamcorper eget nulla facilisi etiam. Et
+							magnis dis parturient montes. Felis eget nunc lobortis mattis aliquam faucibus purus in.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
